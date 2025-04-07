0137a2eb0d3856f6ac019e101709170c176aea9f
amponinJa1 <jamesandreiamponin18@gmail.com>
2025-04-07 16:18:17 +0800

    Update haha.java

--- haha.java
+++ haha.java
@@ -1,18 +1,6 @@
 public class haha {
-    sad
-    'sdas e
-    qsad qa
-    sd qad as 
-    wADA QRDAS
-    C 3QDRFAS BQSD 
-    QW RWQD 
-    QWRDWQA D
-    ASD ASFASD
-    ]FVAS GFADS
-    FAS FSAFASR Fd
-     fweasfra sf
-    ]
+   
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("POGI MO AMPO!");
     }
 }
