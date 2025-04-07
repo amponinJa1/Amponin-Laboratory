26f7eca293207905c2b39e026dd7b6acc4a05b69
Your Name <you@example.com>
2025-04-07 18:15:47 +0800

    aray

---
+++ Act1/Analyst.java
@@ -0,0 +1,14 @@
+package Act1;
+public class Analyst extends Employee {
+    private String department;
+
+    public Analyst(String name,  int age,  double salary) {
+        super(name, age, salary);
+    }
+     
+    public double getAnnualBonus() {
+        return super.salary * 0.5;
+    }
+
+    
+}
---
+++ Act1/SalesPerson.java
@@ -0,0 +1,17 @@
+package Act1;
+public class SalesPerson extends Employee {
+   
+    private double comissionPercentage;
+    public SalesPerson(String name, int age, double salary, double comissionPercentage)  {
+        super(name, age, ,salary);
+        this.comissionPercentage = comissionPercentage; 
+    }    
+    public double getComissionPercentage() {
+        return comissionPercentage;
+    }
+    public void raiseComission() {
+       if (this.comissionPercentage < .30){
+        this.comissionPercentage = this.comissionPercentage * 1.2;
+       }
+    }     
+}
